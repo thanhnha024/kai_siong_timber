5e50ac4e1268da62b0cf83194b80cde1f18de312
toanpham2111 <toan444666@gmail.com>
2024-05-24 09:36:19 +0700

    Styling product page and custom breadcrumb

--- src/wp-content/themes/carservice-child/functions.php
+++ src/wp-content/themes/carservice-child/functions.php
@@ -31,3 +31,41 @@ foreach (glob(THEME_DIR . '-child' . "/includes/*.php") as $file_name) {
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+
+
+// Remove product cat base
+add_filter('term_link', 'devvn_no_term_parents', 1000, 3);
+function devvn_no_term_parents($url, $term, $taxonomy) {
+    if($taxonomy == 'product_cat'){
+        $term_nicename = $term->slug;
+        $url = trailingslashit(get_option( 'home' )) . user_trailingslashit( $term_nicename, 'category' );
+    }
+    return $url;
+}
+// Add our custom product cat rewrite rules
+function devvn_no_product_cat_parents_rewrite_rules($flash = false) {
+    $terms = get_terms( array(
+        'taxonomy' => 'product_cat',
+        'post_type' => 'product',
+        'hide_empty' => false,
+    ));
+    if($terms && !is_wp_error($terms)){
+        foreach ($terms as $term){
+            $term_slug = $term->slug;
+            add_rewrite_rule($term_slug.'/?$', 'index.php?product_cat='.$term_slug,'top');
+            add_rewrite_rule($term_slug.'/page/([0-9]{1,})/?$', 'index.php?product_cat='.$term_slug.'&paged=$matches[1]','top');
+            add_rewrite_rule($term_slug.'/(?:feed/)?(feed|rdf|rss|rss2|atom)/?$', 'index.php?product_cat='.$term_slug.'&feed=$matches[1]','top');
+        }
+    }
+    if ($flash == true)
+        flush_rewrite_rules(false);
+}
+add_action('init', 'devvn_no_product_cat_parents_rewrite_rules');
+/*Fix creat taxonomy 404*/
+add_action( 'create_term', 'devvn_new_product_cat_edit_success', 10);
+add_action( 'edit_terms', 'devvn_new_product_cat_edit_success', 10);
+add_action( 'delete_term', 'devvn_new_product_cat_edit_success', 10);
+function devvn_new_product_cat_edit_success( ) {
+    devvn_no_product_cat_parents_rewrite_rules(true);
+}
