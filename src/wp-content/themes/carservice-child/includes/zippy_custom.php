b980eae4e3546d037f456633c076f32d81b56d4d
shin <nhathanh024@gmail.com>
2024-05-27 17:02:23 +0700

    rebase code

--- src/wp-content/themes/carservice-child/includes/zippy_custom.php
+++ src/wp-content/themes/carservice-child/includes/zippy_custom.php
@@ -2,7 +2,7 @@
 add_action('wp_enqueue_scripts', 'shin_scripts');
 function shin_scripts()
 {
-    $version = '2.2.0';
+    $version = '2.3.0';
 
     // Load CSS
     wp_enqueue_style('main-style-css', THEME_URL . '-child' . '/assets/main/main.css', array(), $version, 'all');
