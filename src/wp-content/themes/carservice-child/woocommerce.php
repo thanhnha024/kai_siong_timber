037b069116fe82513d63a9488e0a8bbef71d11f6
toanpham2111 <toan444666@gmail.com>
2024-05-28 14:36:47 +0700

    Update page shop

--- src/wp-content/themes/carservice-child/woocommerce.php
+++ src/wp-content/themes/carservice-child/woocommerce.php
@@ -1,62 +1,71 @@
 <?php
 get_header();
+global $post;
+$post = get_post(get_option("woocommerce_shop_page_id"));
+setup_postdata($post);
+?>
+	<div class="theme-page padding-bottom-70">
+		<div class="vc_row wpb_row vc_row-fluid gray full-width page-header vertical-align-table">
+			<div class="vc_row wpb_row vc_inner vc_row-fluid">
+				<div class="page-header-left">
+					<h1><?php the_title(); ?></h1>
+				</div>
+				<div class="page-header-right">
+					<div class="bread-crumb-container">
+						<label><?php _e("YOU ARE HERE:", 'carservice'); ?></label>
+						<?php woocommerce_breadcrumb(); ?>
+					</div>
 
-$current_category = get_queried_object();
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="clearfix">
+		<div class="vc_row wpb_row vc_row-fluid margin-top-70">
+			<?php if (!is_single()) :?>
+				<?php
+				if (is_active_sidebar('sidebar-shop')) {
+					$sidebar = cs_get_page_by_title("Sidebar Shop", "carservice_sidebars");
+				}
+				?>
+				<?php
+				if (is_active_sidebar('sidebar-shop')) {
+					if (isset($sidebar) && !(int)get_post_meta($sidebar->ID, "hidden", true)) {
+				?>
+						<div class="vc_col-sm-3 wpb_column vc_column_container<?php echo ((int)get_post_meta($sidebar->ID, "hide_on_mobiles", true) ? ' hide-on-mobiles' : ''); ?>">
+							<div class="wpb_wrapper">
+								<div class="wpb_widgetised_column wpb_content_element clearfix">
+									<div class="wpb_wrapper">
+										<?php dynamic_sidebar('sidebar-shop'); ?>
+									</div>
+								</div>
+							</div>
+						</div>
+				<?php
+					}
+				}
+				?>
+			<?php endif;
+			?>
 
-if ($current_category && is_a($current_category, 'WP_Term')) {
-    $current_category_id = $current_category->term_id;
+			<div class="vc_col-sm-<?php echo (is_active_sidebar('sidebar-shop') && isset($sidebar) && !(int)get_post_meta($sidebar->ID, "hidden", true) ? '9' : '12'); ?> wpb_column vc_column_container">
+				<div class="wpb_wrapper">
+					<?php
+					if (have_posts()) :
+						woocommerce_content();
+					else :
+						_e("No products found", 'carservice');
+					endif;
+					?>
+				</div>
+			</div>
 
-    $sub_args = array(
-        'taxonomy'   => 'product_cat',
-        'parent'     => $current_category_id,
-        'hide_empty' => false,
-        'exclude'    => array(get_term_by('slug', 'uncategorized', 'product_cat')->term_id),
-    );
-    $sub_categories = get_terms($sub_args);
-
-    if (!empty($sub_categories)) {
-        echo '<div class="theme-page padding-bottom-70">';
-        echo '<div class="vc_row wpb_row vc_row-fluid gray full-width page-header vertical-align-table">';
-        echo '<div class="vc_row wpb_row vc_inner vc_row-fluid">';
-        echo '<div class="page-header-left">';
-        echo '<h1>' . $current_category->name . '</h1>';
-        echo '</div>';
-        echo '<div class="page-header-right">';
-        echo '<div class="bread-crumb-container">';
-        echo '<label>' . __("YOU ARE HERE:", 'carservice') . '</label>';
-        woocommerce_breadcrumb();
-        echo '</div>';
-        echo '</div>';
-        echo '</div>';
-        echo '</div>';
-        echo '</div>';
-		echo '<div class="vc_row wpb_row vc_row-fluid">';
-		echo '<div class="wpb_column vc_column_container vc_col-sm-12">';
-		echo '<div class="wpb_wrapper">';
-		echo '<div class="wpb_text_column wpb_content_element ">';
-		echo '<div class="wpb_wrapper">';
-        echo '<ul class="sub-categories">';
-        foreach ($sub_categories as $sub_category) {
-            $sub_thumbnail_id = get_term_meta($sub_category->term_id, 'thumbnail_id', true);
-            $sub_image_url = ($sub_thumbnail_id) ? wp_get_attachment_url($sub_thumbnail_id) : '/wp-content/uploads/woocommerce-placeholder.png';
-
-            echo '<li class="sub-category">';
-            echo '<a href="' . get_term_link($sub_category) . '">';
-            echo '<img src="' . $sub_image_url . '" alt="' . $sub_category->name . '">';
-            echo '<span class="title-sub-category">' . $sub_category->name . '</span>';
-            echo '</a>';
-            echo '</li>';
-        }
-        echo '</ul>';
-		echo '</div>';
-		echo '</div>';
-		echo '</div>';
-		echo '</div>';
-		echo '</div>';
-    } else {
-        echo cs_get_theme_file("/woocommerce-shop.php");
-    }
-}?>
+		</div>
+	</div>
+</div>
 <?php
+global $post;
+$post = get_post(get_option("woocommerce_shop_page_id"));
+setup_postdata($post);
 get_footer();
 ?>
